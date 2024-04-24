5fd2607d6d6780fc918c0515af6f014af6371192
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2024-04-24 22:08:43 +0530

    parallax background

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import "./style/parallax.css";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
+
+        {/* cursor follower */}
+        <div className="cursor"></div>
+
+        {/* stars background */}
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
     </React.StrictMode>,
 )
