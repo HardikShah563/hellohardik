b9e91db545787422550a57b4087af73299bafb8f
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2024-04-24 22:29:52 +0530

    cursor activated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+
 export default function App() {
+    const cursor = document.querySelector('.cursor');
+    useEffect(() => {
+        document.addEventListener("mousemove", (e) => {
+            let leftPosition = e.pageX;
+            let topPosition = e.pageY;
+            cursor.style.left = leftPosition + "px";
+            cursor.style.top = topPosition + "px";
+        });
+    })
+
     return (
-        <></>
+        <>
+            {/* cursor follower */}
+            <div className="cursor"></div>
+
+            {/* stars background */}
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+        </>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-
+// importing css
+import "./style/globals.css";
 import "./style/parallax.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
-
-        {/* cursor follower */}
-        <div className="cursor"></div>
-
-        {/* stars background */}
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
     </React.StrictMode>,
 )
